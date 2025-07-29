87bd5424e5fff27c01b5002ee9faba995922b1d7
Daniela Iglesias <digle032@fiu.edu>
2025-07-29 02:46:07 -0400

    add5

--- generateHash.js
+++ generateHash.js
@@ -1,13 +1,22 @@
 // generateHash.js
 const bcrypt = require('bcrypt');
 
-const password = 'adminpass'; // Change this to the password you want to hash
+const adminPassword = 'adminpass';
+const userPassword = 'userpass';
 const saltRounds = 10;
 
-bcrypt.hash(password, saltRounds, (err, hash) => {
+bcrypt.hash(adminPassword, saltRounds, (err, adminHash) => {
   if (err) {
-    console.error('Error hashing password:', err);
+    console.error('Error hashing admin password:', err);
   } else {
-    console.log('Hashed password:', hash);
+    console.log('✅ Admin hashed password:', adminHash);
+
+    bcrypt.hash(userPassword, saltRounds, (err, userHash) => {
+      if (err) {
+        console.error('Error hashing user password:', err);
+      } else {
+        console.log('✅ User hashed password:', userHash);
+      }
+    });
   }
 });
